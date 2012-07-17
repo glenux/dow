360ac27596752c3bfbaf7760ababaae796e553bc
Glenn Y. Rolland <glenux@glenux.net>
2012-07-17 02:32:57 +0200

    Improved output of Ocaml Makefile.

--- OCaml.mk
+++ OCaml.mk
@@ -13,14 +13,20 @@ OPTS+=
 INCS+=
 LIBS+=
 
+V=0
+ifeq ($(V),1)
+Q=
+else
+Q=@
+endif
+
 define PROGRAM_template
 ALL_OBJS   += $($(1)_OBJS)
 $(1): $$($(1)_OBJS:=.cmx)
-	@echo -n -e "\x1B[31;1m"
+	@printf "\033[31;1m"
 	@echo "[L] $(1)"
-	@echo -n -e "\x1B[0m"
-	$(OCAMLOPT) $($(1)_OPTS) $(OPTS) $(INCS) $(LIBS) $($(1)_INCS) $($(1)_LIBS:=.cmxa) $($(1)_OBJS:=.cmx) -o $(1)
-	@echo ""
+	@printf "\033[0m"
+	$(Q)$(OCAMLOPT) $($(1)_OPTS) $(OPTS) $(INCS) $(LIBS) $($(1)_INCS) $($(1)_LIBS:=.cmxa) $($(1)_OBJS:=.cmx) -o $(1)
 endef
 
 $(foreach prog,$(PROGRAMS),$(eval $(call PROGRAM_template,$(prog))))
@@ -33,41 +39,41 @@ doc:
 	ocamldoc $(INCS) -d doc -html $(ML) $(MLI)
 
 %.cmi: %.mli
-	@echo -n -e "\x1B[31;1m"
+	@printf "\033[31;1m"
 	@echo "[I] $<"
-	@echo -n -e "\x1B[0m"
-	$(OCAMLOPT) $(OPTS) $(INCS) $(LIBS) -i $<
-	$(OCAMLOPT) $(OPTS) $(INCS) $(LIBS) -c $<
-	@echo ""
+	@printf "\033[0m"
+	$(Q)test $(V) -ne 1 || $(OCAMLOPT) $(OPTS) $(INCS) $(LIBS) -i $<
+	$(Q)$(OCAMLOPT) $(OPTS) $(INCS) $(LIBS) -c $<
+	@test $(V) -ne 1 || echo ""
 
 %.cmx: %.ml
-	@echo -n -e "\x1B[31;1m"
+	@printf "\033[31;1m"
 	@echo "[C] $<"
-	@echo -n -e "\x1B[0m"
-	$(OCAMLOPT) $(OPTS) $(INCS) $(LIBS) -i $<
-	$(OCAMLOPT) $(OPTS) $(INCS) $(LIBS) -c $<
-	@echo ""
+	@printf "\033[0m"
+	$(Q)test $(V) -ne 1 || $(OCAMLOPT) $(OPTS) $(INCS) $(LIBS) -i $<
+	$(Q)$(OCAMLOPT) $(OPTS) $(INCS) $(LIBS) -c $<
+	@test $(V) -ne 1 || echo ""
 
 %.cmo %.cmi: %.ml %.cmi %.mli
 	@echo "[O] $<"
-	$(OCAMLC) $(OPTS) $(INCS) $(LIBS) -i $<
-	$(OCAMLC) $(OPTS) $(INCS) $(LIBS) -c $<
-	echo ""
+	$(Q)test $(V) -ne 1 || $(OCAMLC) $(OPTS) $(INCS) $(LIBS) -i $<
+	$(Q)$(OCAMLC) $(OPTS) $(INCS) $(LIBS) -c $<
+	@test $(V) -ne 1 || echo ""
 
 %.cmo %.cmi: %.ml
-	@echo -n -e "\x1B[31;1m"
+	@printf "\033[31;1m"
 	@echo "[O] $<"
-	@echo -n -e "\x1B[0m"
-	$(OCAMLC) $(OPTS) $(INCS) $(LIBS) -i $<
-	$(OCAMLC) $(OPTS) $(INCS) $(LIBS) -c $<
-	echo ""
+	@printf "\033[0m"
+	$(Q)test $(V) -ne 1 || $(OCAMLC) $(OPTS) $(INCS) $(LIBS) -i $<
+	$(Q)$(OCAMLC) $(OPTS) $(INCS) $(LIBS) -c $<
+	@test $(V) -ne 1 || echo ""
 
 clean:
-	rm -f $(PROGRAMS) *~ *.cm* *.o *.a *.so .depend *.cmxa *.cma
+	$(Q)rm -f $(PROGRAMS) *~ *.cm* *.o *.a *.so .depend *.cmxa *.cma
 
 .depend: $(ML) $(MLI)
-	$(OCAMLDEP) $(ML) $(MLI) > .depend
-	@echo ""
+	$(Q)$(OCAMLDEP) $(ML) $(MLI) > .depend
+	@test $(V) -ne 1 || echo ""
 
 
 .SUFFIXES:
